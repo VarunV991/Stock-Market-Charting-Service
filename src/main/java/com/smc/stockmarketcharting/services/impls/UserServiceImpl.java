4f471773f886084cf7f5c6df1b0c7a01fb427244
VarunV991 <varunofficial99@gmail.com>
2021-07-26 12:24:04 +0530

    Changes for send grid mail

--- src/main/java/com/smc/stockmarketcharting/services/impls/UserServiceImpl.java
+++ src/main/java/com/smc/stockmarketcharting/services/impls/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package com.smc.stockmarketcharting.services.impls;
 
+import com.sendgrid.*;
 import com.smc.stockmarketcharting.controllers.UserController;
 import com.smc.stockmarketcharting.dtos.UserDto;
 import com.smc.stockmarketcharting.mappers.UserMapper;
@@ -9,6 +10,7 @@ import com.smc.stockmarketcharting.services.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import javax.mail.Message;
@@ -18,6 +20,7 @@ import javax.mail.Transport;
 import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
+import java.io.IOException;
 import java.util.Optional;
 import java.util.Properties;
 
@@ -32,51 +35,38 @@ public class UserServiceImpl implements UserService {
     @Autowired
     UserMapper userMapper;
 
+    @Value("${sendgrid.api.key}")
+    String sendGridApiKey;
+
     @Override
-    public UserDto addUser(UserDto userDto) throws AddressException, MessagingException {
+    public UserDto addUser(UserDto userDto){
         User user = userMapper.toUser(userDto);
         user = userRepository.save(user);
         return userMapper.toUserDto(user);
     }
 
     @Override
-    public void sendEmail(Long userid) throws AddressException, MessagingException{
+    public void sendEmail(Long userid){
         logger.debug(Long.toString(userid));
         User user = userRepository.findById(userid).get();
-        final String username = "smcroot7@gmail.com";
-        final String password = "root@123";
 
-        Properties prop = new Properties();
-        prop.put("mail.smtp.host", "smtp.gmail.com");
-        prop.put("mail.smtp.port", "587");
-        prop.put("mail.smtp.auth", "true");
-        prop.put("mail.smtp.starttls.enable", "true"); //TLS
+        Email from = new Email("smcroot7@gmail.com");
+        String subject = "User Confirmation Mail";
+        Email to = new Email(user.getEmail());
+        Content content = new Content("text/html",
+                "<h3><a href =\"https://smc-service.herokuapp.com/user/confirmuser/"+userid+"/\"> Click to confirm </a></h3>");
+        Mail mail = new Mail(from, subject, to, content);
 
-        Session session = Session.getInstance(prop,
-                new javax.mail.Authenticator() {
-                    protected javax.mail.PasswordAuthentication getPasswordAuthentication() {
-                        return new javax.mail.PasswordAuthentication(username, password);
-                    }
-                });
-
+        SendGrid sg = new SendGrid(sendGridApiKey);
+        Request request = new Request();
         try {
-
-            Message message = new MimeMessage(session);
-            message.setFrom(new InternetAddress("smcroot7@gmail.com"));
-            message.setRecipients(
-                    Message.RecipientType.TO,
-                    InternetAddress.parse(user.getEmail())
-            );
-            message.setSubject("User confirmation email");
-            message.setContent(
-                    "<h3><a href =\"https://smc-service.herokuapp.com/user/confirmuser/"+userid+"/\"> Click to confirm </a></h3>",
-                    "text/html");
-            Transport.send(message);
-
-            System.out.println("Done");
-
-        } catch (MessagingException e) {
-            e.printStackTrace();
+            request.setMethod(Method.POST);
+            request.setEndpoint("mail/send");
+            request.setBody(mail.build());
+            Response response = sg.api(request);
+            logger.info(response.getBody());
+        } catch (IOException ex) {
+            ex.printStackTrace();
         }
     }
 
